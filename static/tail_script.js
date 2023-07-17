7ef2d400234c8e20ee525e29c3792624969ab70d
Seapoe1809 <129666906+seapoe1809@users.noreply.github.com>
2023-07-17 06:54:39 -0400

    Add files via upload

---
+++ static/tail_script.js
@@ -0,0 +1,7 @@
+
+// Get the link element by its ID
+document.addEventListener('DOMContentLoaded', function() {
+  var link = document.getElementById('dynamic-link');
+  link.href = "https://github.com/pnmeka/Darna_Tailscale_card";
+});
+
---
+++ static/xmr_script.js
@@ -0,0 +1,7 @@
+
+// Get the link element by its ID
+document.addEventListener('DOMContentLoaded', function() {
+  var link = document.getElementById('dynamic-link2');
+  link.href = "https://github.com/pnmeka/XMR_card";
+});
+
